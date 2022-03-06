f505ef60494fafbadddf99372792bdf80d9f61d4
Roger Díaz Viñolas <rdvdev2@gmail.com>
2022-03-06 15:24:41 +0100

    1.18.2 compatibility (v2.0)

--- src/main/java/com/rdvdev2/disablecustomworldsadvice/mixin/MixinMinecraftClient.java
+++ src/main/java/com/rdvdev2/disablecustomworldsadvice/mixin/MixinMinecraftClient.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 public abstract class MixinMinecraftClient {
 
     @ModifyVariable(
-            method = "startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/DynamicRegistryManager$Impl;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V",
+            method = "startIntegratedServer(Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Function;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V",
             at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient$WorldLoadAction;NONE:Lnet/minecraft/client/MinecraftClient$WorldLoadAction;", ordinal = 0),
             ordinal = 2,
             index = 11,

fb75d88bd089c38efc1129b5daa1bcf488112bba
Nara Díaz Viñolas <me@rdvdev2.com>
2023-03-18 19:45:51 +0100

    Fix world creation mixin and simplify world load mixin

--- src/main/java/com/rdvdev2/disablecustomworldsadvice/mixin/MixinIntegratedServerLoader.java
+++ src/main/java/com/rdvdev2/disablecustomworldsadvice/mixin/MixinIntegratedServerLoader.java
@@ -1,46 +1,32 @@
 package com.rdvdev2.disablecustomworldsadvice.mixin;
 
-import com.mojang.serialization.Lifecycle;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.server.integrated.IntegratedServerLoader;
-import net.minecraft.world.SaveProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(IntegratedServerLoader.class)
 public abstract class MixinIntegratedServerLoader {
 
-    // The target method only calls SaveProperties::getLifecycle() to determine if it should show a backup screen. We
-    // can redirect this method to never return Lifecycle::experimental(), disabling the screen for custom worlds, but
-    // keeping it for legacy ones.
-    @Redirect(
+    // Set canShowBackupPrompt = false
+    @ModifyVariable(
             method = "start(Lnet/minecraft/client/gui/screen/Screen;Ljava/lang/String;ZZ)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/SaveProperties;getLifecycle()Lcom/mojang/serialization/Lifecycle;"),
-            require = 1
+            at = @At("HEAD"),
+            argsOnly = true,
+            index = 4
     )
-    private Lifecycle removeAdviceOnLoad(SaveProperties properties) {
-        Lifecycle original = properties.getLifecycle();
-        if (original == Lifecycle.stable() || original == Lifecycle.experimental()) {
-            return Lifecycle.stable();
-        } else {
-            return original;
-        }
+    private boolean removeAdviceOnLoad(boolean original) {
+        return false;
     }
 
-    // The target method contains two calls to MinecraftClient::setScreen() that show the backup screen. The first one
-    // (the one we're targeting) only gets called when we have a custom world and not a legacy one. As this call is at
-    // the end of the method, we can safely inject a call to start the server without showing the screen and return.
-    @Inject(
+    // Set bypassWarnings = true
+    @ModifyVariable(
             method = "tryLoad",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V", ordinal = 0),
-            cancellable = true
+            at = @At("HEAD"),
+            argsOnly = true,
+            index = 4
     )
-    private static void removeAdviceOnCreation(MinecraftClient client, CreateWorldScreen parent, Lifecycle lifecycle, Runnable loader, CallbackInfo ci) {
-        loader.run();
-        ci.cancel();
+    private static boolean removeAdviceOnCreation(boolean original) {
+        return true;
     }
 }
